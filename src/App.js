09d4c42d1738861f682ea3a484105d6efbab2bf8
Lexigal1984 <babygal8407@hotmail.com>
2023-08-30 20:38:34 -0400

    complete MVP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'; // Import the Movie component
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movies, setMovies] = useState([]);
 
@@ -14,23 +17,31 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
+          setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
+    };
     getMovies();
   }, []);
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
+    if (!saved.includes(id)) {
+      setSaved([...saved, id]);
+    }
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList list={saved} />
 
-      <div>Replace this Div with your Routes</div>
+        <Routes>
+          <Route path="/" element={<MovieList movies={movies} addToSavedList={addToSavedList} />} />
+          <Route path="movies/:id" element={<Movie />} />
+        </Routes>
+
     </div>
   );
 }
